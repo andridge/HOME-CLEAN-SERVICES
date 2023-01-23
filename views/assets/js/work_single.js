425a834d5466ec2f5d622314a7d32f9a2b3d1a95
Andridge Munene <andridgemunene@MAC.local>
2023-01-23 15:56:19 +0300

    essage0129

--- views/assets/js/work_single.js
+++ views/assets/js/work_single.js
@@ -1,7 +1,18 @@
 (function (){
     "use strict";
 window.addEventListener('load',()=>{
+//
 
+    var map = L.map('map').setView([-1.289677156299649, 36.81696336220971], 16);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+    L.marker([-1.289677156299649, 36.81696336220971]).addTo(map)
+        .bindPopup(`I Need Services Here`)
+        .openPopup();
+
+
+//
     var service= localStorage.getItem("oneservice");
     var Service =JSON.parse(service);
     console.log(Service);
@@ -9,6 +20,7 @@ window.addEventListener('load',()=>{
     var div2 = `<div>${Service.serviceDetails}</div>`
     document.getElementById("serviceName").insertAdjacentHTML("beforeend",div);
     document.getElementById("serviceDetails").insertAdjacentHTML("beforeend",div2);
+
 })
 
 
